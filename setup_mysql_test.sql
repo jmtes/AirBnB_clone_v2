0510995e1ac5be5cb28b4d05941050bcf7a775c3
jmtes <juliennetesoro@gmail.com>
2019-08-13 19:15:58 +0000

    Add test server setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- Prepare a MySQL server.
--- Check if database hbnb_dev_db exists and if not, create it.
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Check if user 'hbnb_dev' exists and if not, create them.
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
--- Set password for 'hbnb_dev'.
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
--- Grant all privileges on 'hbnb_dev_db' to 'hbnb_dev'.
-GRANT ALL PRIVILEGES ON `hbnb_dev_db` . * TO 'hbnb_dev'@'localhost';
--- Grant select privileges on 'performance_schema' to 'hbnb_dev'.
-GRANT SELECT ON `performance_schema` . * TO 'hbnb_dev'@'localhost';
+-- Check if database hbnb_test_db exists and if not, create it.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Check if user 'hbnb_test' exists and if not, create them.
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- Set password for 'hbnb_test'.
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Grant all privileges on 'hbnb_test_db' to 'hbnb_test'.
+GRANT ALL PRIVILEGES ON `hbnb_test_db` . * TO 'hbnb_test'@'localhost';
+-- Grant select privileges on 'performance_schema' to 'hbnb_test'.
+GRANT SELECT ON `performance_schema` . * TO 'hbnb_test'@'localhost';
