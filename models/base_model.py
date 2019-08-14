8b150bfbd58e9df97953cdc3073ba8d9d12126ee
Svcg17 <sofiavictoria17@gmail.com>
2019-08-14 02:39:52 +0000

    Updating files

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,24 @@
 import uuid
 import models
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
 
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
         Args:
-            args: it won't be used
+            args: it won't used
             kwargs: arguments for the constructor of the BaseModel
         Attributes:
             id: unique id generated
@@ -29,7 +36,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """returns a string
@@ -48,6 +54,7 @@ class BaseModel:
         """updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -59,4 +66,13 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in my_dict:
+            del my_dict['_sa_instance_state']
         return my_dict
+
+    def delete(self):
+        """Delete the current instance from the storage models.storage
+        """
+        models.storage.delete(self)
+
+
--- models/state.py
+++ models/state.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import Base
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship("City", cascade="delete")
+    else:
+        @property
+        def cities(self):
+            """Returns cities obj
+            """
+            newlist = []
+            r =  models.storage.all(City)
+            for key, value in r.items():
+                if 'state_id' in value:
+                    if value[state_id] == self.id:
+                        newlist.append(value)
+            return (newlist)
