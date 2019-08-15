5a704aed05f1d2ca0520be891332145c13317ad2
Svcg17 <sofiavictoria17@gmail.com>
2019-08-15 01:17:10 +0000

    Fixed type error

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,7 +6,8 @@ from models.state import State
 from models.city import City
 from models.user import User
 from sqlalchemy import create_engine
-from models.places import Places
+from models.place import Place
+from models.review import Review
 import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
@@ -32,6 +33,7 @@ class DBStorage:
             query += self.__session.query(City).all()
             query += self.__session.query(User).all()
             query += self.__session.query(Place).all()
+            query += self.__session.query(Review).all()
             for i in query:
                 key = i.__class__.__name__ + "." + i.id
                 newdict[key] = i
--- models/user.py
+++ models/user.py
@@ -18,34 +18,3 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship("Place", backref='user', cascade='all, delete-orphan')
-
-
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref='user', cascade='all, delete-orphan')
-
-class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref='user', cascade='all, delete-orphan')
