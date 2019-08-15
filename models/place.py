9ab713e0753f26f94385321d6519738cde3e9af5
Svcg17 <sofiavictoria17@gmail.com>
2019-08-15 01:41:46 +0000

    Import review

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 import os
+from models.review import Review
 from sqlalchemy.orm import relationship
 
 
