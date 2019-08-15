b7f14f9cf7afba3be5a0098fa89f0757209476a5
jmtes <juliennetesoro@gmail.com>
2019-08-15 18:08:24 +0000

    Add conditional for storage type

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
+import os
 
 
 class Amenity(BaseModel, Base):
@@ -14,5 +15,6 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place", secondary=Place.place_amenity, back_populates="amenities")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship("Place", secondary=Place.place_amenity,
+                                       back_populates="amenities")
--- models/place.py
+++ models/place.py
@@ -38,15 +38,15 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60), ForeignKey(
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenity = Table('place_amenity', Base.metadata,
+                              Column('place_id', String(60), ForeignKey(
                                  'places.id'), primary_key=True,
                                  nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'), primary_key=True,
-                                 nullable=False))
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+                              Column('amenity_id', String(60),
+                                     ForeignKey('amenities.id'),
+                                     primary_key=True,
+                                     nullable=False))
         reviews = relationship(
             "Review", backref='place', cascade='all, delete-orphan')
         amenities = relationship(
