95faa0d47954330787283ab0b87ec6845831cfb6
jmtes <juliennetesoro@gmail.com>
2019-08-30 00:52:17 +0000

    Edit init The __init__ method of BaseModel was not adding the  and  attributes automatically if kwargs were provided and those attributes were not present. Handled with conditionals that check for the existence of those attributes in kwargs and it seems to work for now.

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,10 @@ class BaseModel:
                     setattr(self, key, value)
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
