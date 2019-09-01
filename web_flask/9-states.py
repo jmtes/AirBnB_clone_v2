62223fef2d582995f37d99919915bd30a5181a41
jmtes <juliennetesoro@gmail.com>
2019-09-01 19:48:36 +0000

    Add script

---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' Start a Flask web app. '''
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__, template_folder='./templates')
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    ''' Render page with State info. '''
+    if id:
+        state = storage.get_obj('State', id)
+        return render_template('9-states.html', state=state)
+    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_request
+def teardown(exception):
+    ''' Remove current SQLAlchemy session. '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
