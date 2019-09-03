d6224be945d24647d457b32a3d1c1c4af76392b6
jmtes <juliennetesoro@gmail.com>
2019-09-03 20:01:30 +0000

    Modify decorator

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,7 +15,7 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_request
+@app.teardown_appcontext
 def teardown(exception):
     ''' Remove current SQLAlchemy session. '''
     storage.close()
